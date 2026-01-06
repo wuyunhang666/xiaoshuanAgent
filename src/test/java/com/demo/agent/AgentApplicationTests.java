cdee02136e911bf203a72c02f8c829c1679e0082
吴云行 <1309034107@qq.com>
2026-01-06 23:24:13 +0800

    first commit

---
+++ src/test/java/com/demo/agent/AgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
